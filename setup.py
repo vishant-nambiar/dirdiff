b8cef80b93dbb020d9e981d9fde6a4d0b17246b1
Vishant Nambiar <vishantnambiar@gmail.com>
2021-09-02 16:11:09 +0530

    update license keyword in setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     'Development Status :: 4 - Beta',      
     'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License v3.0',  
+    'License :: OSI Approved :: gpl-3.0',  
     'Programming Language :: Python :: 3',     
   ],
 )
